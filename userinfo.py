b39cd9f40fb3404fd340a592dc9bbe48eadca417
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2024-02-29 12:49:24 +0100

    some xtra code because authentication is now a thing

--- userinfo.py
+++ userinfo.py
@@ -1,4 +1,6 @@
 from flask import request
+from authenticationdatabase import authenticationDB
+from globals import authenticateError
 
 class UserInfo:
     def __init__(self, sessioninfo):
@@ -6,7 +8,20 @@ class UserInfo:
         # but this hides tyhis process behind a class
         self.username = 'undefined'
         if sessioninfo != None and sessioninfo.authorization != None:
-            self.username = sessioninfo.authorization['username']
+            if 'token' in sessioninfo.authorization:
+                auth = sessioninfo.authorization.token
+                auth = auth.split()[1]
+                if 'basic//' not in auth:
+                    return authenticateError()
+
+                token = auth.split('basic//')[1]
+                if authenticationDB.tokenExpired(token) == '?':
+                    return authenticateError() 
+                else:                   
+                    self.username = authenticationDB.getUserFromToken()
+            else: 
+                if sessioninfo.authorization['username'] != None:
+                    self.username = sessioninfo.authorization['username']
     
     def __eq__(self, other):
         if not isinstance(other, UserInfo):
