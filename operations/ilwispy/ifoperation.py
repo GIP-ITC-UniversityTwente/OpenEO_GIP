10bc22c0d683009420013b27ce9f664625436218
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2024-04-03 09:12:52 +0200

    wip; not sure how this operation is called from python as 'if' is a reserved word and I could find no example

--- operations/ilwispy/ifoperation.py
+++ operations/ilwispy/ifoperation.py
@@ -2,19 +2,59 @@ from openeooperation import *
 from operationconstants import *
 from constants import constants
 
-def __init__(self):
-        self.loadOpenEoJsonDef('if.json')
+class IfOperation(OpenEoOperation):
+        def __init__(self):
+                self.loadOpenEoJsonDef('if.json')
+                
+                self.kind = constants.PDPREDEFINED
+
+        def checkData(self, data):
+
+                if type(data) is RasterData:
+                        extra = self.constructExtraParams(data, data.temporalExtent, 0)
+                        raster = data.getRaster().rasterImp()
+                        return {'raster' : raster, 'extra' : extra}
+
+        def checkArgument(self, idata):
+                parmValue = None
+                if isinstance(idata, tuple):
+                        data = list(idata)
+                if isinstance(data, list):
+                        dataList = []
+                        for data in p1:
+                                dataList.append(self.checkData(data)  )
+                        parmValue = dataList
+                else:
+                        parmValue = self.checkData(data) 
+
+                return parmValue                                       
+
+        def prepare(self, arguments):
+                ##TODO
+                self.runnable = False
+                if 'serverChannel' in arguments:
+                        toServer = arguments['serverChannel']
+                        job_id = arguments['job_id']
+                condition = arguments['value']['resolved']
+                p1 = arguments['accept']['resolved']
+                p2 = arguments['reject']['resolved']
+                self.parmValue1 = self.checkArgument(p1)
+                self.parmValue2 = self.checkArgument(p2)
         
-        self.kind = constants.PDPREDEFINED
 
-def prepare(self, arguments):
-        ##TODO
-        self.runnable = False
-                         
-        return ''  
+        def run(self,openeojob, processOutput, processInput):
+                if self.runnable:
+                        result = False
+                        if isinstance(self.parmValue1, list):
+                                if len(self.parmValue1) == len(self.parmValue2):
+                                        iter2 = iter(self.parmValue2)
+                                        for data in self.parmValue1:
+                                                if data != next(iter2):
+                                                        result = False
+                                                        break       
+                        return result
+        
+                return createOutput('error', "operation no runnable", constants.DTERROR)  
 
-def run(self,openeojob, processOutput, processInput):
-    if self.runnable:
-         return None
-    
-    return createOutput('error', "operation no runnable", constants.DTERROR)      
+def registerOperation():
+     return IfOperation()    
