e8bd737c5414b88dc3aa19437f23a15c389fe1e0
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2024-05-01 12:57:36 +0200

    new; for the moment inactive as it was difficult to integrate

---
+++ tests/addTestRasters.py
@@ -0,0 +1,67 @@
+import ilwis
+import math
+import numpy as np
+import common
+from rasterdata import RasterData
+import os
+from pathlib import Path
+import constants.constants as cc
+
+def createEmptySmallNumericRaster():
+    grf = ilwis.GeoReference("epsg:4326", ilwis.Envelope("0 25 30 60") , ilwis.Size(15,12))
+    dfNum = ilwis.DataDefinition(ilwis.NumericDomain("code=value"), ilwis.NumericRange(0.0, 1000000.0, 1.0))
+    rc = ilwis.RasterCoverage()
+    rc.setGeoReference(grf)
+    rc.setDataDef(dfNum)
+
+    return rc 
+                    
+def createSmallNumericRasterNLayers(dims, alternate=0):
+    rc = createEmptySmallNumericRaster()
+    rc.setSize(ilwis.Size(15,12,dims))
+    baseSize = 15 * 12
+
+    data =np.empty(baseSize, dtype = np.int64)
+
+    for count in range(0, dims):
+        if ( alternate == 0):
+            for i in range(len(data)):
+                data[i] = (i + count*baseSize) * 10 
+        if ( alternate == 1):                
+            for i in range(len(data)):
+                data[i] = (i + 2*baseSize + 3 * + 2 * math.sin(math.radians(1 + i*count * 10))) * 10
+
+            data[5 * 6] = ilwis.Const.iUNDEF # pos 0,2 is undefined 
+        rc.array2raster(data, count)                 
+             
+                            
+    return rc 
+    
+def getTestRaster(dims):
+    if common.testRaster_openeo != None:
+        return common.testRaster_openeo
+    
+    rc = createSmallNumericRasterNLayers(dims)
+    raster = RasterData()
+    end = '2020-11-' + str((dims+1)*2)
+    extra = {'epsg' : 4326, 'temporalExtent' : ['2020-11-01', end], 'bands' : [{'name' : 'band_01'}]}
+    text = []
+    for i in range(1,dims + 1):
+        begin = '2020-11-' + str(i*2 - 1)
+        end = '2020-11-' + str((i)*2)
+        text.append([begin, end])
+    extra['textsublayers'] = text 
+
+    url = rc.url()
+    path = url.split('//')
+    folder = os.path.dirname("/"+ path[1])
+    path = Path(folder).as_uri()
+    ilwis.setWorkingCatalog(path)  
+    raster.load(rc, 'ilwisraster', extra)
+    raster['name'] = cc.TESTFILENAME
+
+    common.testRaster_openeo = raster
+
+    return raster
+   
+   
