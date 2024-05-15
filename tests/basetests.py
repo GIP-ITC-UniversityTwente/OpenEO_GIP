566d11e97d46843099c94cdfc748ebcc9e43f7ae
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2024-05-15 09:37:43 +0200

    added

--- tests/basetests.py
+++ tests/basetests.py
@@ -1,14 +1,26 @@
 import unittest
 import configglobals
+from zlib import adler32
+import shutil
+import os
+import json
 
 
+CUSTOM_EX = 'CUSTOM_EX@'
 
+def customError(regular_message, custom_message):
+     if not custom_message.find(CUSTOM_EX) == -1:
+          parts = custom_message.split('@')
+          regular_message = regular_message + "," + parts[1]
+     return regular_message              
+    
 def testExceptionCondition6(p, expected,func, parm1, parm2, parm3, parm4, parm5, parm6, message):
       try:
            obj = func(parm1, parm2, parm3, parm4, parm5, parm6)
            p.isTrue(expected, message)
            return obj
       except Exception as ex:
+           message = customError(message, str(ex))
            p.isTrue(not expected, message)
 
 def testExceptionCondition5(p, expected, func, parm1, parm2, parm3, parm4, parm5, message):
@@ -17,6 +29,7 @@ def testExceptionCondition5(p, expected, func, parm1, parm2, parm3, parm4, parm5
            p.isTrue(True, expected)
            return obj
       except Exception as ex:
+           message = customError(message, str(ex))
            p.isTrue(True, not expected)
            return False
 
@@ -26,6 +39,7 @@ def testExceptionCondition4(p, expected, func, parm1, parm2, parm3, parm4, messa
            p.isTrue(expected, message)
            return obj
       except Exception as ex:
+           message = customError(message, str(ex))
            p.isTrue(not expected, message)
            return None
 
@@ -35,6 +49,7 @@ def testExceptionCondition3(p, expected, func, parm1, parm2, parm3, message):
            p.isTrue(expected, message)
            return obj
       except Exception as ex:
+           message = customError(message, str(ex))
            p.isTrue(not expected, message)
            return None
 
@@ -44,6 +59,7 @@ def testExceptionCondition2(p, expected, func, parm1, parm2, message):
            p.isTrue(expected, message)
            return obj
       except Exception as ex:
+           message = customError(message, str(ex))
            p.isTrue(not expected, message)
 
 def testExceptionCondition1(p, expected, func, parm1, message):
@@ -52,8 +68,61 @@ def testExceptionCondition1(p, expected, func, parm1, message):
            p.isTrue(expected, message)
            return obj
       except Exception as ex:
+           message = customError(message, str(ex))
            p.isTrue(not expected, message)
-           return None           
+           return None 
+
+def checksum(p): 
+     BLOCKSIZE=256*1024*1024
+     asum = 1
+     with open(p,mode="rb") as f:
+          while True:
+               data = f.read(BLOCKSIZE)
+               if not data:
+                    break
+               asum = adler32(data, asum)
+               if asum < 0:
+                    asum += 2**32
+
+               return hex(asum)[2:10].zfill(8).lower()   
+
+def testCheckSum(group, operation, name):
+     testdir = '/home/mschouwen/testdata/openeo/result'
+     #group = 'aggregate'
+     testdiragg = testdir + "/" + group
+     if os.path.exists(name):
+          file_stats = os.stat(name) # sometimes if an error occurs openeo still creates an empty result file
+          if file_stats.st_size == 0:
+               raise Exception('file not found')
+
+          if not os.path.exists(testdiragg):
+               os.makedirs(testdiragg)
+          checksumfile = testdir + "/checksums.txt"
+          data = {}
+          if os.path.exists(checksumfile):
+               file_stats = os.stat(checksumfile)
+               if file_stats.st_size > 0:
+                    with open(checksumfile, 'r') as fp:
+                         data = json.load(fp)
+               else: # probably rest of an aborted test
+                    os.remove(checksumfile)               
+          testfile = testdiragg + "/"+ name        
+          if os.path.exists(testfile):        
+               os.remove(testfile)
+          shutil.move(name, testdiragg)
+          cc = checksum(testfile)
+
+          key = group + operation
+          if key in data:
+               if not data[key]['checksum'] == cc:
+                    raise Exception(CUSTOM_EX + 'checksum error')
+          else:
+               entry = {'group' : group, 'operation' : operation, 'checksum' : cc}
+               data[key] = entry
+               with open(checksumfile, 'w') as fp:
+                    json.dump(data, fp)     
+     else:
+          raise Exception('file not found')
 
 class BaseTest(unittest.TestCase):
     
@@ -106,7 +175,7 @@ class BaseTest(unittest.TestCase):
             result = 'FAIL'
         print(f'{cls.testCount():5} {msg:65}  {result}')
         
- 
+
    
 
 
