e6ce94868131573f5187bd17fa880c1bdf39c927
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2024-05-16 11:03:05 +0200

    ErrorManager renamed to testmanager as it also takes care of some toher things

--- tests/configglobals.py
+++ tests/configglobals.py
@@ -1,9 +1,11 @@
 import os
 
-class ErrorManager:
+testdir = '/home/mschouwen/testdata/openeo/result'
+
+class TestManager:
     def __new__(cls):
         if not hasattr(cls, 'instance'):
-            cls.instance = super(ErrorManager, cls).__new__(cls)
+            cls.instance = super(TestManager, cls).__new__(cls)
         return cls.instance
     
     def init(self):
@@ -33,5 +35,23 @@ class ErrorManager:
         en = os.environ["OPENEO_ERROR_LIST"]
         en += ":" + str(num)
         os.environ["OPENEO_ERROR_LIST"] = en
+
+
+def clean_subfolders(folder_path):
+# Iterate over all items (files and subfolders) in the specified folder
+    for item in os.listdir(folder_path):
+        item_path = os.path.join(folder_path, item)
+        
+        if os.path.isfile(item_path):
+            if folder_path != testdir: # we dont remove files in the root
+                os.remove(item_path)
+        elif os.path.isdir(item_path):
+            # If it's a subfolder, recursively clean its contents
+            clean_subfolders(item_path)
+
+def cleanup():
+    clean_subfolders(testdir)
+# Iterate over all items (files and subfolders) in the specified folder
+
 
         
