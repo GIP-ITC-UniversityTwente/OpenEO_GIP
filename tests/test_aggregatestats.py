2ea00a1e0c931d7b144e84f8866a75bff3d45b56
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2024-05-22 09:36:55 +0200

    now works in synthetic data

--- tests/test_aggregatestats.py
+++ tests/test_aggregatestats.py
@@ -12,25 +12,19 @@ conn = openeo.connect("http://127.0.0.1:5000")
 conn.authenticate_basic("tester", "pwd") 
 ##conn = openeo.connect("http://cityregions.roaming.utwente.nl:5000")  
 
-def getCube1():
-    cube_s1 = conn.load_collection(
-        "Sentinel2TimeSeriesData" ,
-        spatial_extent =  {"west": -119.2201, "south": 35.959, "east":  -119.0861, "north": 36.0574},
-        temporal_extent =  ["2018-11-18", "2019-10-24"],
-        bands = ['B02']
-    )
-    return cube_s1
+cube_s1 = conn.load_collection(
+    cc.TESTFILENAME1 ,
+    spatial_extent =  {"west": -5, "south": 26, "east":  65, "north":67},
+    temporal_extent =  ["2020-11-03", "2020-11-07"],
+    bands = ['TB01'])
 
-def getCube2():
-    cube_s2 = conn.load_collection(
-        "Sentinel2TimeSeriesData" ,
-        spatial_extent =  {"west": -119.2201, "south": 35.959, "east":  -119.0861, "north": 36.0574},
-        temporal_extent =  ["2018-11-18", "2019-10-24"],
-        bands = ['B02','B04']
-    )
-    return cube_s2
+cube_s2 = conn.load_collection(
+    cc.TESTFILENAME2 ,
+    spatial_extent =  {"west": -5, "south": 26, "east":  65, "north":67},
+    temporal_extent =  ["2020-11-03", "2020-11-07"],
+    bands = ['TB01', 'TB02'])
 
-   
+  
 def execAgg(operation, sdata):
     cube_s2 = sdata.reduce_dimension(dimension="t", reducer=operation )
     name = operation + ".tif"
@@ -42,10 +36,10 @@ class TestAggregateStats(basetests.BaseTest):
 
     def test_01_agg_raster(self): 
         self.prepare(sys._getframe().f_code.co_name)
-        cubedata=getCube2()
+        cubedata = cube_s2
         basetests.testExceptionCondition1(self, False, lambda r1 : execAgg(operation=r1, sdata=cubedata), 'sum',"two result, one download. must fail. sum")
        
-        cubedata=getCube1()
+        cubedata =cube_s1
         basetests.testExceptionCondition1(self, True, lambda r1 : execAgg(operation=r1, sdata=cubedata), 'sum',"Aggregate stats. sum")
         basetests.testExceptionCondition1(self, True, lambda r1 : execAgg(operation=r1, sdata=cubedata), 'mean',"Aggregate stats. mean")
         basetests.testExceptionCondition1(self, True, lambda r1 : execAgg(operation=r1, sdata=cubedata), 'max',"Aggregate stats. max")
