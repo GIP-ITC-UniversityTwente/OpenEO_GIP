637f45e601a709da5ddbee98f582925b46631db3
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2024-05-22 09:36:33 +0200

    now works on synthetic data

--- tests/test_math_raster.py
+++ tests/test_math_raster.py
@@ -4,7 +4,7 @@ from tests import basetests
 import openeo
 import sys
 import openeo.rest.datacube
-from openeo.processes import ProcessBuilder, add, multiply, divide, subtract
+from openeo.processes import ProcessBuilder, add, multiply, divide, subtract, log
 import openeo.processes as oeo
 import constants.constants as cc
 
@@ -12,42 +12,34 @@ import constants.constants as cc
 conn = openeo.connect("http://127.0.0.1:5000")
 conn.authenticate_basic("tester", "pwd") 
 ##conn = openeo.connect("http://cityregions.roaming.utwente.nl:5000")  
-'''
-cube_s1 = conn.load_collection(
-    "Sentinel2TimeSeriesData" ,
-    spatial_extent =  {"west": -119.2201, "south": 35.959, "east":  -119.0861, "north": 36.0574},
-    temporal_extent =  ["2018-11-18", "2019-10-24"],
-    bands = ['B02','B04']
-)
-'''
 
-cube_s1 = conn.load_collection(
-    "SYNTHETIC_DATA1" ,
+cube_s3 = conn.load_collection(
+    cc.TESTFILENAME1 ,
     spatial_extent =  {"west": -5, "south": 26, "east":  65, "north":67},
     temporal_extent =  ["2020-11-03", "2020-11-07"],
     bands = ['TB01']
 )
-band_selection = 'cube_s1.band("TB01")'
+band_selection = 'cube_s3.band("TB01")'
 
 def execUnaryMathRaster(oper, name):
     expr = 'oeo.' + oper + '(' + band_selection + ')'
-    cube_s2 = cube_s1.apply(lambda : eval(expr))
+    cube_c4 = cube_s3.apply(lambda : eval(expr))
     name = name + ".tif"
-    cube_s2.download(name)
+    cube_c4.download(name)
     basetests.testCheckSum('math', name, name)
 
 def execBinaryMathRasterAlt(oper, name):
     expr = oper + '(' + band_selection + ', 10)'
-    new_cube = cube_s1.apply(lambda : eval(expr))
+    new_cube = cube_s3.apply(lambda : eval(expr))
     name = name + ".tif"
     new_cube.download(name)
     basetests.testCheckSum('math', oper, name)
 
 def execBinaryMathRaster(oper, name):
-    expr = band_selection + oper + '10' 
-    cube_s2 = eval(expr)
+    expr = band_selection + oper + '3.45' 
+    cube_s4 = eval(expr)
     name = name + ".tif"
-    cube_s2.download(name)
+    cube_s4.download(name)
     basetests.testCheckSum('math', name, name)
 
 class TestMathRaster(basetests.BaseTest):
@@ -64,7 +56,11 @@ class TestMathRaster(basetests.BaseTest):
         basetests.testExceptionCondition1(self, True, lambda r1: execBinaryMathRasterAlt('subtract', 'subtractalt'), 'subtract alt',"band math. subtract alt int")
 
         basetests.testExceptionCondition1(self, True, lambda r1: execBinaryMathRaster('/', 'divide'), 'divide',"band math. divide int")
-        basetests.testExceptionCondition1(self, True, lambda r1: execBinaryMathRasterAlt('divide', 'dividealt'), 'divide alt',"band math. divide alt int")                        
+        basetests.testExceptionCondition1(self, True, lambda r1: execBinaryMathRasterAlt('divide', 'dividealt'), 'divide alt',"band math. divide alt int") 
+
+        basetests.testExceptionCondition1(self, True, lambda r1: execBinaryMathRasterAlt('log', 'log'), 'log',"band math. log")                       
+        basetests.testExceptionCondition1(self, True, lambda r1: execBinaryMathRaster('>', 'greater'), 'greater',"band math.greater") 
+        basetests.testExceptionCondition1(self, True, lambda r1: execBinaryMathRaster('<', 'less'), 'less',"band math. less") 
 
     def test_02_unary_math_raster(self): 
         self.prepare(sys._getframe().f_code.co_name) 
