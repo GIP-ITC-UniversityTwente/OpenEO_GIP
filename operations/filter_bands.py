c6377c1ead1d9221f15fd74a9d7ebb3d11c73e89
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2024-05-28 11:09:48 +0200

    finished the application

--- operations/filter_bands.py
+++ operations/filter_bands.py
@@ -9,6 +9,8 @@ class FilterBands(OpenEoOperation):
 
     def prepare(self, arguments):
         self.runnable = False 
+        self.bandsByWavelength = []    
+        self.bandsByName = []   
         if 'serverChannel' in arguments:
             toServer = arguments['serverChannel']
             job_id = arguments['job_id']
@@ -20,31 +22,56 @@ class FilterBands(OpenEoOperation):
             return message         
         if isinstance(self.inpData[0], RasterData):
             if 'bands' in arguments:
-                requestedBands = arguments['eo:bands']['resolved']
-                foundCount = 0
-                for item in self.inpData:
-                    for bandItem in item['eo:bands']:
-                        if bandItem['name'] in requestedBands:
-                            foundCount = foundCount + 1
-                if foundCount == len(requestedBands):
-                    self.bands = requestedBands                    
-                    self.runnable = True
-                else:
-                    message =  'Band list doesn match available bands'
-                    common.logMessage(logging.ERROR, message,common.process_user)
-                    self.handleError(toServer, job_id, 'bands',message, 'ProcessParameterInvalid')                   
-            if 'wavelenghts' in arguments:
-                requestedWavelengths = arguments['wavelengths']['resolved']
+                requestedBands = arguments['bands']['resolved']
+                if len(requestedBands) > 0:
+                    foundCount = 0
+                    for item in self.inpData:
+                        for bandItem in item['eo:bands'].items():
+                            if bandItem[1]['name'] in requestedBands or bandItem[1]['commonbandname'] in requestedBands:
+                                self.bandsByName.append(item)
+                                foundCount = foundCount + 1
+                    if foundCount != len(requestedBands):
+                        message =  'Band list doesn match available bands'
+                        common.logMessage(logging.ERROR, message,common.process_user)
+                        self.handleError(toServer, job_id, 'bands',message, 'ProcessParameterInvalid')                   
+            if 'wavelengths' in arguments:
+                wavelengths = arguments['wavelengths']['resolved']
+                if len(wavelengths) > 0:
+                    for subset in wavelengths:
+                        if len(subset) != 2:
+                            self.handleError(toServer, job_id, 'filter bands', 'All subset of wavelength values must have 2 values(min, max)', 'ProcessParameterInvalid')
+                        minv = subset[0]
+                        maxv = subset[1]
+                        isinstance(minv, (int, float))
+                        if isinstance(minv, (int, float)) and isinstance(maxv, (int, float)): 
+                            if minv > maxv:
+                                self.handleError(toServer, job_id, 'filter bands', 'All subset of wavelength values must be ordered min..max', 'ProcessParameterInvalid')                                    
+                        else:
+                            self.handleError(toServer, job_id, 'filter bands', 'Wavelength values must be numerical', 'ProcessParameterInvalid') 
+                                                                                                                                       
+                        for item in self.inpData:
+                            for bandItem in item['eo:bands'].items():
+                                cwavelength = bandItem[1].getDetail(RasterBand.CENTER_WAVELENGTH)
+                                if cwavelength >= minv  and maxv >= cwavelength:
+                                    self.bandsByWavelength.append(item)
+            self.runnable = True                                 
+                
 
     def run(self,openeojob , processOutput, processInput):
         if self.runnable:
             self.logStartOperation(processOutput, openeojob)
             outData = []
-            for raster in self.inpData:
-                    for bandItem in raster['eo:bands']:
-                        if self.bands != None:
-                            if bandItem['name'] in self.bands:
-                                outData.append(raster)
+            for item in self.bandsByName:
+                outData.append(item)
+            for item in self.bandsByWavelength:
+                found = False
+                for existingItem in outData:
+                    if existingItem['id'] == item['id']:
+                        found = True
+                        break
+                if not found:
+                    outData.append(item)
+
             self.logEndOperation(processOutput,openeojob)
             return createOutput(constants.STATUSFINISHED, outData, constants.DTRASTER)
         common.notRunnableError(self.name, openeojob.job_id) 
