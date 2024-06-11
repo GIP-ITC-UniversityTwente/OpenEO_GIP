9fe39545567081155c077cb8276aa84e5cd21e29
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2024-06-11 10:20:17 +0200

    improved

--- tests/test_reduce_dimension.py
+++ tests/test_reduce_dimension.py
@@ -13,20 +13,28 @@ cube_s2 = conn.load_collection(
     temporal_extent =  ["2020-11-03", "2020-11-07"])
 
 
-def execReduceDimension(operation, nm):
-    name = operation + ".tif"
-    b2 = cube_s2.reduce_dimension(dimension="t", reducer=nm )
+def execReduceDimension(name, reduc):
+    name = name + ".tif"
+    b2 = cube_s2.reduce_dimension(dimension="t", reducer=reduc )
     result = b2.save_result("GTiff")
     job = result.create_job()
     job.start_and_wait()
     job.get_results().download_files(name) 
     basetests.testCheckSumMulti('reduce_dimension', name)       
    
+def ndif(data):
+	TB01 = array_element(data, index = 1)
+	TB03 = array_element(data, label = "TB03") # or a label
 
+	ndvi = normalized_difference(TB01, TB03)
+
+	return ndvi
 
 class TestReduceDimension(basetests.BaseTest):
 
     def test_01_add_dimension(self): 
         self.prepare(sys._getframe().f_code.co_name)
 
-        basetests.testExceptionCondition1(self, True, lambda r1 : execReduceDimension(operation=r1, nm='mean'), 'adddimensionb',"add a simpel dimension")
+        basetests.testExceptionCondition1(self, True, lambda r1: execReduceDimension(name='mean1', reduc='mean'), 'reducedimensiona',"reduce a dimension using aggregate method")
+        basetests.testExceptionCondition1(self, True, lambda r1: execReduceDimension(name='udf1', reduc=ndif), 'reducedimensionb',"reduce a dimension using udf method") 
+        basetests.testExceptionCondition1(self, False, lambda r1: execReduceDimension(name='mean1', reduc='dummy123'), 'reducedimensionc',"reduce a dimension using not existing method, fail")       
