b0ced41b0c6c13cb8ae123af760e33adf4e771c1
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2024-06-20 11:05:02 +0200

    extended

--- tests/test_mask.py
+++ tests/test_mask.py
@@ -18,6 +18,16 @@ cube_s3 = conn.load_collection(
     spatial_extent =  {"west": -5, "south": 26, "east":  65, "north":67},
     temporal_extent =  ["2020-11-03", "2020-11-07"])
 
+cube_s4 = conn.load_collection(
+    cc.TESTFILENAME4 ,
+    spatial_extent =  {"west": 10, "south": 25, "east":  30, "north":60},
+    temporal_extent =  ["2020-11-03", "2020-11-07"])
+
+cube_s5 = conn.load_collection(
+    cc.TESTFILENAME4 ,
+    spatial_extent =  {"west": 100, "south": 25, "east":  300, "north":60},
+    temporal_extent =  ["2020-11-03", "2020-11-07"])
+
    
 def execAddDimension(operation):
     name = operation + ".tif"
@@ -26,7 +36,25 @@ def execAddDimension(operation):
     job = result.create_job()
     job.start_and_wait()
     job.get_results().download_files(name) 
-    basetests.testCheckSumMulti('mask', name)       
+    basetests.testCheckSumMulti('mask', name)
+
+def execAddDimension2(operation):
+    name = operation + ".tif"
+    b2 = cube_s2.apply(lambda : mask(cube_s2, cube_s4, 100))
+    result = b2.save_result("GTiff")
+    job = result.create_job()
+    job.start_and_wait()
+    job.get_results().download_files(name) 
+    basetests.testCheckSumMulti('mask', name) 
+
+def execAddDimension3(operation):
+    name = operation + ".tif"
+    b2 = cube_s2.apply(lambda : mask(cube_s2, cube_s5, 100))
+    result = b2.save_result("GTiff")
+    job = result.create_job()
+    job.start_and_wait()
+    job.get_results().download_files(name) 
+    basetests.testCheckSumMulti('mask', name)                    
    
 
 
@@ -36,3 +64,5 @@ class TestMask(basetests.BaseTest):
         self.prepare(sys._getframe().f_code.co_name)
 
         basetests.testExceptionCondition1(self, True, lambda r1 : execAddDimension(operation=r1), 'mask1','simpel mask')
+        basetests.testExceptionCondition1(self, True, lambda r1 : execAddDimension2(operation=r1), 'mask2','simpel mask, shifted')
+        basetests.testExceptionCondition1(self, False, lambda r1 : execAddDimension3(operation=r1), 'mask3','mask, not fitting, fail')
