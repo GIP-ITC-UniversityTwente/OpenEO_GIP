e4ae187bf4e983aca73ad805ba9bab81d5c7bd07
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2024-06-27 13:06:39 +0200

    bg

--- tests/test_aggregate_temporal.py
+++ tests/test_aggregate_temporal.py
@@ -23,6 +23,12 @@ cube_s2 = conn.load_collection(
     temporal_extent =  ["2020-11-01", "2020-12-10"],
     bands = ['TB01', 'TB02'])
 
+cube_smany = conn.load_collection(
+    cc.TESTFILENAME_MANY_LAYERS ,
+    spatial_extent =  {"west": -5, "south": 26, "east":  65, "north":67},
+    temporal_extent =  ["2020-11-01", "2021-02-28"],
+    bands = ['TB01', 'TB02'])
+
 def execAgg(operation, ti, sdata):
     if sdata == []:
         cube_s3 = cube_s2.aggregate_temporal( ti, reducer = 'mean')
@@ -32,15 +38,30 @@ def execAgg(operation, ti, sdata):
     job = result.create_job()
     job.start_and_wait()
     job.get_results().download_files(operation) 
-    basetests.testCheckSumMulti('aggregate_temporal', operation)           
+    basetests.testCheckSumMulti('aggregate_temporal', operation)
+
+def execAgg2(operation, ti, sdata):
+    if sdata == []:
+        cube_s3 = cube_smany.aggregate_temporal_period( ti, reducer = 'mean')
+    else:
+        cube_s3 = cube_smany.aggregate_temporal( ti, reducer = 'mean', labels = sdata)        
+    result = cube_s3.save_result("GTiff")
+    job = result.create_job()
+    job.start_and_wait()
+    job.get_results().download_files(operation) 
+    basetests.testCheckSumMulti('aggregate_temporal', operation)                          
 
 class TestAggregateSTemp(basetests.BaseTest):
 
-    def test_01_agg_temp(self): 
+    def test_01_agg_temp_period(self):
+        basetests.testExceptionCondition1(self, True, lambda r1 : execAgg2(operation=r1, ti='dekad', sdata=[]), 'aggtempperiod1',"Aggregate stats period.") 
+
+    def test_02_agg_temp(self): 
         self.prepare(sys._getframe().f_code.co_name)
 
         basetests.testExceptionCondition1(self, True, lambda r1 : execAgg(operation=r1, ti= [["2020-11-03", "2020-11-07"], ['2020-11-13', '2020-11-23']], sdata=[]), 'aggtemp1',"Aggregate stats.") 
         basetests.testExceptionCondition1(self, True, lambda r1 : execAgg(operation=r1, ti= [["2020-11-03", "2020-11-07"], ['2020-11-13', '2020-11-23']], sdata=['t1', 't2']), 'aggtemp2',"Aggregate stats, labels match.") 
         basetests.testExceptionCondition1(self, False, lambda r1 : execAgg(operation=r1, ti= [["2020-11-03", "2020-11-07"]], sdata=['t1', 't2']), 'aggtemp3',"Aggregate stats, no match labels. fail")         
         basetests.testExceptionCondition1(self, False, lambda r1 : execAgg(operation=r1, ti= [["2000-11-03", "2000-11-07"], ['2000-11-13', '2000-11-23']], sdata=[]), 'aggtemp4',"Aggregate stats. no overlap intervals, fail")         
-        basetests.testExceptionCondition1(self, False, lambda r1 : execAgg(operation=r1, ti= [], sdata=[]), 'aggtemp5',"empty intervals")         
+        basetests.testExceptionCondition1(self, False, lambda r1 : execAgg(operation=r1, ti= [], sdata=[]), 'aggtemp5',"empty intervals")                 
+
