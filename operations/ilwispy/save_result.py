211d23f4a9fe598a71afbce667e1c7d2a1c525eb
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2024-07-02 09:04:18 +0200

    can create output dir by itself now as there were cases where this was not possible

--- operations/ilwispy/save_result.py
+++ operations/ilwispy/save_result.py
@@ -3,7 +3,7 @@ from operationconstants import *
 from constants import constants
 from common import openeoip_config
 import re
-import pathlib
+
 
 class SaveResultOperation(OpenEoOperation):
     def __init__(self):
@@ -24,34 +24,39 @@ class SaveResultOperation(OpenEoOperation):
         if self.runnable:
             self.logStartOperation(processOutput, openeojob)
             filePath = openeoip_config['data_locations']['root_user_data_location']
-            filePath = filePath['location'] + '/' + str(openeojob.job_id)    
-            os.makedirs(filePath)
+            filePath = filePath['location'] + '/' + str(openeojob.job_id) 
+            if not os.path.exists(filePath):   
+                os.makedirs(filePath)
 
             env = ilwis.Envelope()
             count = 1
             if self.data != None:
-                for d in self.data:
-                    name = d['title'] 
-                    name = name.replace('_ANONYMOUS', 'raster')                    
-                    for raster in d[DATAIMPLEMENTATION].values():
-                        outpath = filePath + '/' + name + "_"+ str(count)
-                        raster.store("file://" + outpath,self.format, "gdal")
-                        envTemp = raster.envelope()
-                        if not env:
-                            env = envTemp
-                        else:
-                            env.add(envTemp)
-                        count = count + 1                        
-                
+                if isinstance(self.data, list):
+                    for d in self.data:
+                        if isinstance(d, RasterData):
+                            name = d['title'] 
+                            name = name.replace('_ANONYMOUS', 'raster')                    
+                            for raster in d[DATAIMPLEMENTATION].values():
+                                outpath = filePath + '/' + name + "_"+ str(count)
+                                raster.store("file://" + outpath,self.format, "gdal")
+                                envTemp = raster.envelope()
+                                if not env:
+                                    env = envTemp
+                                else:
+                                    env.add(envTemp)
+                                count = count + 1                        
+                    
 
-                ext = ('.tif','.dat','.mpr','.tiff','.jpg', '.png')
-                file_names = [f for f in os.listdir(filePath) if f.endswith(ext)]
-                files = []
-                for filename in file_names:
-                    fn = filePath + "/"  + filename
-                    files.append(fn)
-            self.logEndOperation(processOutput,openeojob)
-            outputInfo =  createOutput(constants.STATUSFINISHED, files, constants.DTRASTERLIST)
+                            ext = ('.tif','.dat','.mpr','.tiff','.jpg', '.png')
+                            file_names = [f for f in os.listdir(filePath) if f.endswith(ext)]
+                            files = []
+                            for filename in file_names:
+                                fn = filePath + "/"  + filename
+                                files.append(fn)
+                        self.logEndOperation(processOutput,openeojob)
+                        outputInfo =  createOutput(constants.STATUSFINISHED, files, constants.DTRASTERLIST)
+                else:
+                    outputInfo = createOutput(constants.STATUSFINISHED, self.data, constants.DTNUMBER)                       
             if env:
                 parts = re.split("[\s,]+", str(env))
                 outputInfo['spatialextent'] = parts
