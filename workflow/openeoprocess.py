370159d530407b4f026b55e26cf01c36fd986f8a
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2024-07-09 13:23:38 +0200

    only non raster htings can be directly registerd as assets

--- workflow/openeoprocess.py
+++ workflow/openeoprocess.py
@@ -280,7 +280,8 @@ class OpenEOProcess(multiprocessing.Process):
                 dict = self.toDict(False) 
                 dict['start_datetime']  = timeStart
                 dict['end_datetime']  = timeEnd
-                dict["assets"] = outputinfo
+                if outputinfo['datatype'] == constants.DTRASTER  or outputinfo['datatype'] == constants.DTRASTERLIST: 
+                    dict["assets"] = outputinfo
 
                 if not os.path.exists(filedir): # this the case were not save_result was part of the workflow  
                     os.makedirs(filedir)
