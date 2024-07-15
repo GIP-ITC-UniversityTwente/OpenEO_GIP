464506cfa4bd80b7ce81afc174cbc35c2f0754bd
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2024-07-15 09:19:46 +0200

    wip

--- start_tests.py
+++ start_tests.py
@@ -18,7 +18,7 @@ loader = unittest.TestLoader()
 cls = configglobals.TestManager()
 cls.init()
 
-tests = loader.discover('./tests','test_filter_bb*.py')
+tests = loader.discover('./tests','test_apply_k*.py')
 testRunner = unittest.runner.TextTestRunner()
 testRunner.run(tests)
 
