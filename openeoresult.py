4c37d8ff1fb266ee7f554f3c2e8abfd1fa144a88
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2025-04-10 11:10:30 +0200

    removed some unneeded stuff

--- openeoresult.py
+++ openeoresult.py
@@ -5,30 +5,12 @@ from workflow.openeoprocess import OpenEOProcess
 from userinfo import UserInfo
 from processmanager import makeBaseResponseDict
 from authentication import AuthenticatedResource
-import pathlib
 import logging
 import common
-import datetime
 from constants import constants
 from customexception import CustomException
-import ilwis
-import os
-import glob
 
-def getMimeType(filename):
-    try:
-        ext = pathlib.Path(filename).suffix
-        if ext == '.jpg':
-            return "image/jpeg"
-        elif ext == '.tif' or ext == '.tiff':
-            return 'image/tiff'
-        elif ext == '.png':
-            return "image/png" 
-        return 'application/octet-stream'
-    except:
-        return 'application/octet-stream'
-          
-        
+
 class OpenEOIPResult(AuthenticatedResource):
     def post(self):
         request_json = request.get_json()
@@ -36,11 +18,6 @@ class OpenEOIPResult(AuthenticatedResource):
         try:
             process = OpenEOProcess(user, request_json,0)
             
-            ''' seems the client already sends a validate request befor trying to run it so this one is superflous
-            errors =  process.validate()
-            if len(errors) > 0:
-                raise Exception(str(errors))
-            '''
             common.logMessage(logging.INFO, 'started sync: ' + process.job_id , common.process_user)
             if process.processGraph != None:
                 outputInfo = process.processGraph.run(process, None, None)
@@ -56,8 +33,6 @@ class OpenEOIPResult(AuthenticatedResource):
                 return make_response(makeBaseResponseDict(process.job_id, 'error', code, None, message),int(code))
             return make_response(makeBaseResponseDict(-1, 'error', 404, None, str(ex)),400)
 
-   
-        
 
     def makeType(self, tp):
         if ( tp == DTNUMBER):
